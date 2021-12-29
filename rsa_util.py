ec4f79a1bba1dea4e36017c9182b420cf645844c
backspace-phill <psusemiehl@gmail.com>
2021-12-29 17:38:43 +0100

    Added en- and decryption

--- rsa_util.py
+++ rsa_util.py
@@ -2,7 +2,8 @@
 import argparse
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
-
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 def rsa_gen_key(keysize:int):
     priv= rsa.generate_private_key(public_exponent=65537, key_size=keysize)
     return(priv)
@@ -11,18 +12,25 @@ def rsa_serialize_keys(private, public):
     pub_serial = private.public_key().public_bytes(encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
     return(priv_serial,pub_serial)
 
-def rsa_enc(cyphertext:str,pub_key):
+def rsa_enc(input_stream,pub_key):
+    ciphertext = pub_key.encrypt(input_stream,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
 
+    return(ciphertext)
 
-    print("rsa_enc")
-
-def rsa_dec():
-    print("rsa_dec")
+def rsa_dec(input_stream, priv_key):
+    plaintext = priv_key.decrypt(input_stream,padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),algorithm=hashes.SHA256(),label=None))
+    return(plaintext)
 
 def main():
     # Das ist die Main Routine
     print("MAIN")
-    print(rsa_serialize_keys(rsa_gen_key(2048), rsa_gen_key(2048).public_key()))
+    key = rsa_gen_key(2048)
+    pub_key = key.public_key()
+    # print(rsa_serialize_keys(rsa_gen_key(2048), rsa_gen_key(2048).public_key()))
+    encrypted = rsa_enc(b"HALLO_DAS_IST_EIN_TEST", pub_key)
+    print(encrypted)
+    decrypted = rsa_dec(encrypted, key)
+    print(decrypted)
 
 
 if __name__ == '__main__':
