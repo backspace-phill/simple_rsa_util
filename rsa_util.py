67670386a8fce2ba9da455d979ba56f2daa74676
backspace-phill <psusemiehl@gmail.com>
2021-12-29 16:47:32 +0100

    Little bit of stuff added

---
+++ rsa_util.py
@@ -0,0 +1,23 @@
+# import cryptography
+import argparse
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+def rsa_gen_key(keysize:int):
+    print("rsa_gen_key")
+
+    return(keysize)
+
+
+def rsa_enc():
+    print("rsa_enc")
+
+def rsa_dec():
+    print("rsa_dec")
+
+def main():
+    # Das ist die Main Routine
+    print("MAIN")
+
+
+if __name__ == '__main__':
+    main()
